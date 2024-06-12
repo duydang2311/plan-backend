0c1f801c8920d65944cfaee12d509fdd3cc18197
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-06-12 20:20:01 +0700

    feat: add case insensitive collation

---
+++ WebApp.Host/Migrations/20240612125329_002_case_insensitive.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class _002_case_insensitive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                    create collation case_insensitive(provider = icu, locale = 'und-u-ks-level2', deterministic = false);
+                """
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                    drop collation case_insensitive;
+                """
+            );
+        }
+    }
+}
