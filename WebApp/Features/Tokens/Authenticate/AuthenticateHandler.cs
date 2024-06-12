dccecf2b75c463337c7802869db5b214e0b96f3b
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-06-12 20:25:13 +0700

    refactor: real auth

--- WebApp/Features/Tokens/Authenticate/AuthenticateHandler.cs
+++ WebApp/Features/Tokens/Authenticate/AuthenticateHandler.cs
@@ -1,21 +1,44 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using OneOf;
+using WebApp.SharedKernel.Hashers.Abstractions;
 using WebApp.SharedKernel.Jwts.Abstractions;
 using WebApp.SharedKernel.Models;
+using WebApp.SharedKernel.Persistence;
 
 namespace WebApp.Features.Tokens.Authenticate;
 
 using Result = OneOf<IEnumerable<ValidationError>, AuthenticateResult>;
 
-public sealed class AuthenticateHandler(IOptions<JwtOptions> options, IJwtService jwtService)
-    : ICommandHandler<AuthenticateCommand, Result>
+public sealed class AuthenticateHandler(
+    IOptions<JwtOptions> options,
+    IJwtService jwtService,
+    IHasher hasher,
+    AppDbContext dbContext
+) : ICommandHandler<AuthenticateCommand, Result>
 {
     public async Task<Result> ExecuteAsync(AuthenticateCommand command, CancellationToken ct)
     {
-        if (command is not { Email: "test@gmail.com", Password: "123456" })
+        var user = await dbContext
+            .Users.Where(a => EF.Functions.Collate(a.Email, "case_insensitive") == command.Email)
+            .Select(a => new
+            {
+                a.Id,
+                a.Salt,
+                a.PasswordHash
+            })
+            .FirstOrDefaultAsync(ct)
+            .ConfigureAwait(false);
+
+        if (user is null)
+        {
+            return new[] { new ValidationError("email", "Email is not found", "email_not_found"), };
+        }
+
+        if (!hasher.Verify(command.Password, user.PasswordHash, user.Salt))
         {
             return new[]
             {
@@ -31,7 +54,7 @@ public sealed class AuthenticateHandler(IOptions<JwtOptions> options, IJwtServic
         var accessToken = jwtService.CreateToken(
             issuer: o.ValidIssuers.FirstOrDefault(),
             audience: o.ValidAudiences.FirstOrDefault(),
-            claims: [new Claim(JwtRegisteredClaimNames.Sub, command.Email)],
+            claims: [new Claim(JwtRegisteredClaimNames.Sub, user.Id.Value.ToString("D"))],
             notBefore: now,
             expires: now.Add(accessTokenMaxAge),
             issuedAt: now
