a5474edde33cce76a783e46876d193eefc8a810c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-07-13 16:49:52 +0700

    refactor: use Permit

--- WebApp/Features/Workspaces/HasWorkspace/HasWorkspaceHandler.cs
+++ WebApp/Features/Workspaces/HasWorkspace/HasWorkspaceHandler.cs
@@ -1,6 +1,7 @@
 using Casbin;
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
+using WebApp.SharedKernel.Constants;
 using WebApp.SharedKernel.Models;
 using WebApp.SharedKernel.Persistence;
 
@@ -27,7 +28,7 @@ public sealed class HasWorkspaceHandler(AppDbContext dbContext, IEnforcer enforc
         }
 
         return await enforcer
-            .EnforceAsync(command.UserId.ToString(), workspace.Id.ToString(), workspace.Id.ToString(), "read")
+            .EnforceAsync(command.UserId.ToString(), workspace.Id.ToString(), workspace.Id.ToString(), Permit.Read)
             .ConfigureAwait(false);
     }
 }
