1b30d240855fb704657913c770e3f63a6d723cec
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-09-14 19:08:58 +0700

    refactor: rename Status.Order to Rank

---
+++ WebApp.Api.V1/Migrations/20240914120623_032_status_rank.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class _032_status_rank : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "order",
+                table: "statuses",
+                newName: "rank");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "rank",
+                table: "statuses",
+                newName: "order");
+        }
+    }
+}
--- WebApp/Domain/Entities/Status.cs
+++ WebApp/Domain/Entities/Status.cs
@@ -3,7 +3,7 @@ namespace WebApp.Domain.Entities;
 public sealed record Status
 {
     public StatusId Id { get; init; }
-    public int Order { get; init; }
+    public int Rank { get; init; }
     public string Value { get; init; } = string.Empty;
     public string Color { get; init; } = string.Empty;
     public string? Description { get; init; }
