1bd5f9442b5ff51699378bff69593a29a1150216
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-09-18 22:50:16 +0700

    feat: add Status.Icon

---
+++ WebApp.Api.V1/Migrations/20240918153209_039_statuses_icon.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class _039_statuses_icon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "icon",
+                table: "workspace_statuses",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "icon",
+                table: "workspace_statuses");
+        }
+    }
+}
--- WebApp/Domain/Entities/Status.cs
+++ WebApp/Domain/Entities/Status.cs
@@ -6,5 +6,6 @@ public abstract record Status
     public int Rank { get; init; }
     public string Value { get; init; } = string.Empty;
     public string Color { get; init; } = string.Empty;
+    public string? Icon { get; init; }
     public string? Description { get; init; }
 }
