f7e645b0cf417a6d16653797869c81c312005631
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-09-19 02:28:30 +0700

    refactor: add WorkspaceId to get workspace statuses response

--- WebApp.Api.V1/[Features]/WorkspaceStatuses/GetMany/Response.cs
+++ WebApp.Api.V1/[Features]/WorkspaceStatuses/GetMany/Response.cs
@@ -9,6 +9,7 @@ public sealed record Response : PaginatedList<Response.Item>
     public sealed record Item
     {
         public StatusId Id { get; init; }
+        public WorkspaceId WorkspaceId { get; init; }
         public int Rank { get; init; }
         public string Value { get; init; } = string.Empty;
         public string Color { get; init; } = string.Empty;
