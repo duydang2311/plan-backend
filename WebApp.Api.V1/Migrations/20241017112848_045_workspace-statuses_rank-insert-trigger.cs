120d80b14567569e92447a2d9b337c52a0be703f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-10-18 02:15:56 +0700

    feat: add before insert trigger for setting workspace status rank

---
+++ WebApp.Api.V1/Migrations/20241017112848_045_workspace-statuses_rank-insert-trigger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class _045_workspacestatuses_rankinserttrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                    create function init_workspace_status() returns trigger as $$
+                    begin
+                        select coalesce(max(rank), 1 << 31) / 2 + ~(1 << 31) / 2 into new.rank from workspace_statuses where workspace_id = new.workspace_id;
+                        return new;
+                    end;
+                    $$ language plpgsql;
+
+                    create trigger TR_workspace_statuses_BI before insert on workspace_statuses
+                    for each row execute function init_workspace_status();
+                """
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                    drop trigger TR_workspace_statuses_BI on workspace_statuses;
+                    drop function init_workspace_status();
+                """
+            );
+        }
+    }
+}
