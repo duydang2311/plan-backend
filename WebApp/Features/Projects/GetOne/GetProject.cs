35576511b619532111ad6386d9065e348f373324
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-11-28 02:16:27 +0700

    refactor(Projects): add WorkspaceId

--- WebApp/Features/Projects/GetOne/GetProject.cs
+++ WebApp/Features/Projects/GetOne/GetProject.cs
@@ -8,6 +8,7 @@ namespace WebApp.Features.Projects.GetOne;
 
 public sealed record GetProject : ICommand<OneOf<ValidationFailures, None, Project>>
 {
+    public WorkspaceId? WorkspaceId { get; init; }
     public ProjectId? ProjectId { get; init; }
     public string? Identifier { get; init; }
     public string? Select { get; init; }
--- WebApp/Features/Projects/GetOne/GetProjectHandler.cs
+++ WebApp/Features/Projects/GetOne/GetProjectHandler.cs
@@ -2,6 +2,7 @@ using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 using OneOf;
 using OneOf.Types;
+using WebApp.Common.Helpers;
 using WebApp.Common.Models;
 using WebApp.Domain.Entities;
 using WebApp.Infrastructure.Persistence;
@@ -13,24 +14,40 @@ public sealed class GetProjectHandler(AppDbContext db)
 {
     public async Task<OneOf<ValidationFailures, None, Project>> ExecuteAsync(GetProject command, CancellationToken ct)
     {
-        if (command.ProjectId is null && string.IsNullOrEmpty(command.Identifier))
-        {
-            return ValidationFailures
-                .Many(2)
-                .Add("projectId", "require fields projectId or identifier", "required_either")
-                .Add("identifier", "require fields projectId or identifier", "required_either");
-        }
-
         var query = db.Projects.AsQueryable();
 
         if (command.ProjectId is not null)
         {
             query = query.Where(a => a.Id == command.ProjectId);
         }
-
-        if (!string.IsNullOrEmpty(command.Identifier))
+        else if (command.WorkspaceId.HasValue && !string.IsNullOrEmpty(command.Identifier))
         {
-            query = query.Where(a => a.Identifier.Equals(command.Identifier));
+            query = query.Where(a => a.WorkspaceId == command.WorkspaceId && a.Identifier.Equals(command.Identifier));
+        }
+        else
+        {
+            return ValidationFailures
+                .Many(3)
+                .Add(
+                    "projectId",
+                    "require fields \"projectId\" or \"workspaceId\" and \"identifier\"",
+                    "required_either"
+                )
+                .Add(
+                    "workspaceId",
+                    "require fields \"projectId\" or \"workspaceId\" and \"identifier\"",
+                    "required_either"
+                )
+                .Add(
+                    "identifier",
+                    "require fields \"projectId\" or \"workspaceId\" and \"identifier\"",
+                    "required_either"
+                );
+        }
+
+        if (!string.IsNullOrEmpty(command.Select))
+        {
+            query = query.Select(ExpressionHelper.Select<Project, Project>(command.Select));
         }
 
         var project = await query.FirstOrDefaultAsync(ct).ConfigureAwait(false);
