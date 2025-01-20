54b149d1c476e5fd26cdb8ff1d667c117bb82025
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-01-20 23:40:13 +0700

    feat: add UserRole.CreatedTime

---
+++ WebApp.Api.V1/Migrations/20250120075439_060_user-roles_created-time.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace WebApp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class _060_userroles_createdtime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Instant>(
+                name: "created_time",
+                table: "user_roles",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: NodaTime.Instant.FromUnixTimeTicks(0L));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_time",
+                table: "user_roles");
+        }
+    }
+}
--- WebApp/Domain/Entities/UserRole.cs
+++ WebApp/Domain/Entities/UserRole.cs
@@ -1,7 +1,10 @@
+using NodaTime;
+
 namespace WebApp.Domain.Entities;
 
 public abstract record UserRole
 {
+    public Instant CreatedTime { get; init; }
     public UserRoleId UserRoleId { get; init; }
     public UserId UserId { get; init; }
     public User User { get; init; } = null!;
