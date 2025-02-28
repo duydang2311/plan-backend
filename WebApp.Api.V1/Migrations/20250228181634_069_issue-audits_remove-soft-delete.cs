b52af9ea44bcd328d673e0931dd85273db094df6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-03-01 01:24:04 +0700

    refactor: remove soft delete from issue audits

---
+++ WebApp.Api.V1/Migrations/20250228181634_069_issue-audits_remove-soft-delete.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace WebApp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class _069_issueaudits_removesoftdelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "deleted_time", table: "issue_audits");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Instant>(
+                name: "deleted_time",
+                table: "issue_audits",
+                type: "timestamp with time zone",
+                nullable: true
+            );
+        }
+    }
+}
