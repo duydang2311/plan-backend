0dd1559f26cd8cb3361171c0b5f32a77eaf4faa3
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-03-21 23:03:31 +0700

    refactor(AppHost): disable mailer for now due to some upstream networking issues

--- WebApp.AppHost/Program.cs
+++ WebApp.AppHost/Program.cs
@@ -9,16 +9,20 @@ var mailerOptions =
     ?? throw new ValidationException("MailerOptions is required");
 Validator.ValidateObject(mailerOptions, new ValidationContext(mailerOptions));
 
-builder.AddContainer("nats", "nats", "latest").WithDockerfile("../").WithContainerRuntimeArgs("--network=host");
+var nats = builder
+    .AddDockerfile("nats", "../")
+    .WithEndpoint(4223, 4223, "ws")
+    .WithEndpoint(4222, 4222, "nats", "nats")
+    .WithEndpoint(8222, 8222, "http");
 
-builder
-    .AddContainer(mailerOptions.Name, mailerOptions.Image, mailerOptions.Tag)
-    .WithDockerfile(mailerOptions.DockerContextPath)
-    .WithContainerRuntimeArgs("--network=host")
-    .WithEnvironment("RESEND_API_KEY", mailerOptions.ResendApiKey)
-    .WithEnvironment("NATS_URL", mailerOptions.NatsUrl)
-    .WithEnvironment("NATS_USER", mailerOptions.NatsUser)
-    .WithEnvironment("NATS_PASSWORD", mailerOptions.NatsPassword);
+// var mailer = builder
+//     .AddDockerfile(mailerOptions.Name, mailerOptions.DockerContextPath)
+//     .WaitFor(nats)
+//     .WithContainerRuntimeArgs("--network=host")
+//     .WithEnvironment("RESEND_API_KEY", mailerOptions.ResendApiKey)
+//     .WithEnvironment("NATS_URL", mailerOptions.NatsUrl)
+//     .WithEnvironment("NATS_USER", mailerOptions.NatsUser)
+//     .WithEnvironment("NATS_PASSWORD", mailerOptions.NatsPassword);
 
 builder.AddProject<Projects.WebApp_Api_V1>("api-v1");
 
