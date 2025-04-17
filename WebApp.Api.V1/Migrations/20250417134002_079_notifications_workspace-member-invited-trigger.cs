c9af16d3af27458421de340715fd8b70f49d4e49
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2025-04-17 22:17:41 +0700

    feat(db): add trigger to delete workspace invitation notification

---
+++ WebApp.Api.V1/Migrations/20250417134002_079_notifications_workspace-member-invited-trigger.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Host.Migrations
+{
+    /// <inheritdoc />
+    public partial class _079_notifications_workspacememberinvitedtrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                create function delete_workspace_invitation_notifications()
+                returns trigger as
+                $$
+                begin
+                    delete from "notifications" where ("data"->>'workspaceInvitationId')::bigint = old.id;
+                    return old;
+                END;
+                $$
+                language plpgsql;
+
+                create TRIGGER TR_workspace_invitations_AD
+                after delete on workspace_invitations
+                for each row
+                execute function delete_workspace_invitation_notifications();
+                """
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                drop trigger TR_workspace_invitations_AD on issues;
+                drop function delete_workspace_invitation_notifications();
+                """
+            );
+        }
+    }
+}
